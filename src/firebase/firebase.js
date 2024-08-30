e19043978509288ef629143973eb93330a146d4d
Jimmy Solis <jimmysolis14@gmail.com>
2024-08-30 00:51:41 -0400

    Initial commit with project files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage"; // Import the storage function
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASgAH8E5UAtQOsTjbAZmLs0nP1NyiqFVw",
-  authDomain: "js-and-jg-database.firebaseapp.com",
-  projectId: "js-and-jg-database",
-  storageBucket: "js-and-jg-database.appspot.com",
-  messagingSenderId: "411197912691",
-  appId: "1:411197912691:web:9d6012a35ddd6e38bc1c06",
-  measurementId: "G-C2C4QFQW65"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -21,5 +21,7 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const storage = getStorage(app); // Initialize Firebase Storage
 
-// Export the storage instance
-export { db,storage, analytics };
+// Export the Firebase services
+export { db, storage, analytics };
+
+
